d27a893382b004cadddbf5f5f84216517ff4d6b2
Reece Pieri <46065144+Icemaush@users.noreply.github.com>
2020-03-23 08:02:14 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace TimeConverterGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.programBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timeLbl1 = new System.Windows.Forms.Label();
+            this.timeLbl2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.form1BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.DiffLbl = new System.Windows.Forms.Label();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Convert from: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.programBindingSource1;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(261, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // programBindingSource1
+            // 
+            this.programBindingSource1.DataSource = typeof(TimeConverterGUI.Program);
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(TimeConverterGUI.Program);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 171);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Time:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Selected Time:";
+            // 
+            // timeLbl1
+            // 
+            this.timeLbl1.AutoSize = true;
+            this.timeLbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLbl1.Location = new System.Drawing.Point(129, 162);
+            this.timeLbl1.Name = "timeLbl1";
+            this.timeLbl1.Size = new System.Drawing.Size(0, 25);
+            this.timeLbl1.TabIndex = 5;
+            // 
+            // timeLbl2
+            // 
+            this.timeLbl2.AutoSize = true;
+            this.timeLbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLbl2.Location = new System.Drawing.Point(129, 203);
+            this.timeLbl2.Name = "timeLbl2";
+            this.timeLbl2.Size = new System.Drawing.Size(0, 25);
+            this.timeLbl2.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DataSource = this.programBindingSource1;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(140, 65);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(261, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // form1BindingSource1
+            // 
+            this.form1BindingSource1.DataSource = typeof(TimeConverterGUI.Form1);
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(TimeConverterGUI.Form1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Convert to: ";
+            // 
+            // DiffLbl
+            // 
+            this.DiffLbl.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.DiffLbl.AutoSize = true;
+            this.DiffLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiffLbl.Location = new System.Drawing.Point(364, 203);
+            this.DiffLbl.Name = "DiffLbl";
+            this.DiffLbl.Size = new System.Drawing.Size(0, 25);
+            this.DiffLbl.TabIndex = 10;
+            // 
+            // timePicker
+            // 
+            this.timePicker.CustomFormat = "0:dd/mm/yyyy H:mm";
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timePicker.Location = new System.Drawing.Point(274, 110);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.ShowUpDown = true;
+            this.timePicker.Size = new System.Drawing.Size(128, 20);
+            this.timePicker.TabIndex = 11;
+            this.timePicker.ValueChanged += new System.EventHandler(this.timePicker_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Select date and time: ";
+            // 
+            // datePicker
+            // 
+            this.datePicker.CustomFormat = "";
+            this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datePicker.Location = new System.Drawing.Point(140, 110);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(128, 20);
+            this.datePicker.TabIndex = 13;
+            this.datePicker.ValueChanged += new System.EventHandler(this.datePicker_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 278);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.timePicker);
+            this.Controls.Add(this.DiffLbl);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timeLbl2);
+            this.Controls.Add(this.timeLbl1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Timezone Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource programBindingSource;
+        private System.Windows.Forms.BindingSource programBindingSource1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label timeLbl1;
+        private System.Windows.Forms.Label timeLbl2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.BindingSource form1BindingSource1;
+        private System.Windows.Forms.Label DiffLbl;
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker datePicker;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeConverterGUI
+{
+    public partial class Form1 : Form
+    {
+        DateTime convertFromDateTime;
+        DateTime convertToDateTime = DateTime.Now;
+        DateTime newDateTime;
+        TimeZoneInfo combo1TZ;
+        TimeZoneInfo combo2TZ;
+        int offsetA = 0;
+        int offsetB = 0;
+        bool comboCheck;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Pull system timezones to use as a datasource for comboboxes.
+            var tzCollection = TimeZoneInfo.GetSystemTimeZones();
+            var bindingsource1 = new BindingSource();
+            bindingsource1.DataSource = tzCollection;
+
+            // Set comboboxes to local time zone.
+            foreach (TimeZoneInfo timeZone in tzCollection)
+            {
+                if (timeZone.Id == TimeZoneInfo.Local.Id)
+                {
+                    int index = tzCollection.IndexOf(timeZone);
+                    
+                    // Bind data source to combobox 1.
+                    comboBox1.DataSource = bindingsource1.DataSource;
+                    comboBox1.SelectedIndex = index;
+                    comboCheck = true;
+
+                    // Create new binding contect and bind datasource to combobox 2.
+                    comboBox2.BindingContext = new BindingContext();
+                    comboBox2.DataSource = bindingsource1.DataSource;
+                    comboBox2.SelectedIndex = index;
+
+                    break;
+                }
+            }
+
+            GetNewDateTime();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboCheck)
+            {
+                GetNewDateTime();
+                CheckDiff();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetNewDateTime();
+            CheckDiff();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckDiff() // Checks the offset between the two selected timezones.
+        {
+            if (offsetB > offsetA)
+            {
+                DiffLbl.ForeColor = System.Drawing.Color.Green;
+                DiffLbl.Text = "+" + Convert.ToString(offsetB - offsetA) + " Hours";
+            } else if (offsetB < offsetA)
+            {
+                DiffLbl.ForeColor = System.Drawing.Color.Red;
+                DiffLbl.Text = Convert.ToString(offsetB - offsetA + " Hours");
+            } else
+            {
+                DiffLbl.Text = "";
+            }
+        }
+
+        private void timePicker_ValueChanged(object sender, EventArgs e)
+        {
+            GetNewDateTime();
+        }
+
+        private void datePicker_ValueChanged(object sender, EventArgs e)
+        {
+            GetNewDateTime();
+        }
+
+        private void GetNewDateTime()
+        {
+            // Update newDateTime variable.
+            newDateTime = DateTime.SpecifyKind(datePicker.Value.Date + timePicker.Value.TimeOfDay, DateTimeKind.Unspecified);
+
+            // Get time zone info from first combobox selection.
+            combo1TZ = (TimeZoneInfo)comboBox1.SelectedItem;
+            offsetA = combo1TZ.BaseUtcOffset.Hours;
+            convertFromDateTime = newDateTime;
+
+            if (comboCheck)
+            {
+                // Get time zone info from second combobox selection.
+                combo2TZ = (TimeZoneInfo)comboBox2.SelectedItem;
+
+                //convertToDateTime = TimeZoneInfo.ConvertTimeFromUtc(newDateTime.ToUniversalTime(), combo2TZ);
+                offsetB = combo2TZ.BaseUtcOffset.Hours;
+                convertToDateTime = TimeZoneInfo.ConvertTime(newDateTime, combo1TZ, combo2TZ);
+            }
+            UpdateLabels();
+        }
+
+        private void UpdateLabels()
+        {
+            timeLbl1.Text = convertFromDateTime.ToString();
+            timeLbl2.Text = convertToDateTime.ToString();
+        }
+    }
+}
